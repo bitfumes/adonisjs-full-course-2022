83dbf9bc87912b98f3e6182314b01ada15ca9ed2
sarthaksavvy <sarthak@bitfumes.com>
2022-07-23 21:50:07 +0530

    06 AdonisJS full course 2022 - route params

--- start/routes.ts
+++ start/routes.ts
@@ -6,7 +6,21 @@ Route.get("/", async ({ view }) => {
 
 Route.on("/news").render("news.view");
 
-Route.post("/news", ({ request, response }) => {
+Route.post("/news", ({ response }) => {
   // const { email, password } = request.body();
   return response.redirect("/news");
+});
+
+Route.patch("/news/:id", ({ params }) => {
+  return { params };
+}).where("id", {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id),
+});
+
+Route.delete("/news/:id", ({ params }) => {
+  return { params };
+}).where("id", {
+  match: /^[0-9]+$/,
+  cast: (id) => Number(id),
 });
