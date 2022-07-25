f75d58f894b5782f8a8dccf23c1c296b8f83e2ff
sarthaksavvy <sarthak@bitfumes.com>
2022-07-25 21:30:22 +0530

    23 AdonisJS full course 2022 - using model

--- app/Controllers/Http/ArticlesController.ts
+++ app/Controllers/Http/ArticlesController.ts
@@ -1,11 +1,12 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Database from "@ioc:Adonis/Lucid/Database";
+import Article from "App/Models/Article";
 import CreateArticleValidator from "App/Validators/CreateArticleValidator";
 
 export default class ArticlesController {
   public async index({ view }) {
-    const articles = await Database.from("articles").select("*");
+    const articles = await Article.all();
     return view.render("article/view", { articles });
   }
 
@@ -14,25 +15,23 @@ export default class ArticlesController {
   }
 
   public async show({ view, params }) {
-    const article = await Database.from("articles")
-      .where("slug", params.slug)
-      .first();
+    const article = await Article.findBy("slug", params.slug);
     return view.render("article/show", { article });
   }
 
   public async store({ response, request }) {
     const payload = await request.validate(CreateArticleValidator);
-    await Database.table("articles").insert({
-      ...payload,
-      slug: payload.title.replace(" ", "-") + +new Date(),
-    });
+    await Article.create(payload);
+    // await Database.table("articles").insert({
+    //   ...payload,
+    //   slug: payload.title.replace(" ", "-") + +new Date(),
+    // });
 
     return response.redirect().back();
   }
 
   public async edit({ view, params }) {
-    const { slug } = params;
-    const article = await Database.from("articles").where("slug", slug).first();
+    const article = await await Article.findBy("slug", params.slug);
     return view.render("article/edit", { article });
   }
 
@@ -43,7 +42,10 @@ export default class ArticlesController {
   }
 
   public async destroy({ params, response }) {
-    await Database.from("articles").where("slug", params.slug).delete();
-    return response.redirect().back();
+    const article = await Article.findBy("slug", params.slug);
+    if (article) {
+      article.delete();
+      return response.redirect().back();
+    }
   }
 }
---
+++ app/Models/Article.ts
@@ -0,0 +1,25 @@
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
+import { DateTime } from "luxon";
+
+export default class Article extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public title: string;
+
+  @column()
+  public slug: string;
+
+  @column()
+  public content: string;
+
+  @column()
+  public image: string;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime;
+}
