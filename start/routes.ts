175115c1142253abbf385075ab303c00d6c565ba
sarthaksavvy <sarthak@bitfumes.com>
2022-07-25 22:37:56 +0530

    27 AdonisJS full course 2022 - login user

--- start/routes.ts
+++ start/routes.ts
@@ -1,4 +1,17 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.on("/").render("welcome");
-Route.resource("news", "ArticlesController").paramFor("news", "slug");
+
+Route.group(() => {
+  Route.resource("news", "ArticlesController").paramFor("news", "slug");
+}).middleware("auth");
+
+Route.on("/login").render("auth.login").as("auth.login");
+
+Route.post("/login", async ({ auth, request, response }) => {
+  const email = request.input("email");
+  const password = request.input("password");
+
+  await auth.use("web").attempt(email, password);
+  return response.redirect("/");
+});
